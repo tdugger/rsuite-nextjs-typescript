85c1c6bf1e7787f89f67f3bc9b650b528605f46c
Troy Dugger <troy@atomicrobot.com>
2022-10-23 21:27:29 -0500

    gets all data

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,17 +13,63 @@ const center = {
   textAlign: 'center'
 };
 
+type Author = {
+  key: string,
+  name: string,
+  works: Work[],
+  topWork: string
+}
 
-const Home = () => { 
-  const [searchAuthorOne, setSearchAuthorOne] = useState('');
-  const [searchAuthorTwo, setSearchAuthorTwo] = useState('');
+type Work = {
+  title: string,
+  revision: number
+};
 
-  const searchForAuthorOne = (event: React.SyntheticEvent<Element, Event>): void => {
-    console.log('searching for author one', searchAuthorOne);
+const getAuthorData = async (authorName: string, numWorks: number): Promise<Author> => {
+  const author: Author = {
+    key: '',
+    name: '',
+    works: [],
+    topWork: ''
+  };
+  //console.log('numWorks:', numWorks);
+  const urlEncodedAuthorName = encodeURIComponent(authorName);
+  const authors = await fetch(`https://openlibrary.org/search/authors.json?q=${urlEncodedAuthorName}`);
+  const authorsData = await authors.json();
+
+  //console.log(authorsData);
+  if (authorsData.numFound && authorsData.numFound > 0) {
+    author.key = authorsData.docs[0].key;
+    author.name = authorsData.docs[0].name;
+    author.topWork = authorsData.docs[0].top_work;
+    //console.log(`top work for ${author.name}`, author.topWork);
+    //console.log('works for author', author.name);
+
+    const worksResponse = await fetch(`https://openlibrary.org/authors/${author.key}/works.json`);
+    const worksData = await worksResponse.json();
+    //console.log(worksData);
+    author.works = worksData.entries;
+  }
+  return author;
+};
+
+
+const Home = () => {
+  const [authorOne, setAuthorOne] = useState('');
+  const [authorTwo, setAuthorTwo] = useState('');
+  const [numberWorks, setNumberWorks] = useState(6);
+  const authors: Author[] = [];
+
+  const searchForAuthorOne = async (event: React.SyntheticEvent<Element, Event>): Promise<void> => {
+    console.log('searching for author one', authorOne);
+    authors[0] = await getAuthorData(authorOne, numberWorks);
+    console.log(authors[0]);
   };
 
-  const searchForAuthorTwo = (event: React.SyntheticEvent<Element, Event>): void => {
-    console.log('searching for author two', searchAuthorTwo);
+  const searchForAuthorTwo = async (event: React.SyntheticEvent<Element, Event>): Promise<void> => {
+    console.log('searching for author two', authorTwo);
+    authors[1] = await getAuthorData(authorTwo, numberWorks);
+    console.log(authors[1]);
   };
 
   return (
@@ -37,23 +83,23 @@ const Home = () => {
       <div className="show-grid">
         <FlexboxGrid justify='center'>
           <FlexboxGrid.Item colspan={4} style={padding}>
-            <InputGroup size="md"> 
-              <Input placeholder={"Author One"} value={searchAuthorOne} onChange={setSearchAuthorOne} onPressEnter={searchForAuthorOne}/>
+            <InputGroup size="md">
+              <Input placeholder={"Author One"} value={authorOne} onChange={setAuthorOne} onPressEnter={searchForAuthorOne} />
               <InputGroup.Button onClick={searchForAuthorOne}>
                 <SearchIcon />
               </InputGroup.Button>
             </InputGroup>
           </FlexboxGrid.Item>
           <FlexboxGrid.Item colspan={4} style={padding}>
-            <InputGroup size="md"> 
-              <Input placeholder={"Author Two"} value={searchAuthorTwo} onChange={setSearchAuthorTwo} onPressEnter={searchForAuthorTwo}/>
+            <InputGroup size="md">
+              <Input placeholder={"Author Two"} value={authorTwo} onChange={setAuthorTwo} onPressEnter={searchForAuthorTwo} />
               <InputGroup.Button onClick={searchForAuthorTwo}>
                 <SearchIcon />
               </InputGroup.Button>
             </InputGroup>
           </FlexboxGrid.Item>
           <FlexboxGrid.Item colspan={4} style={padding}>
-              <InputNumber size="md" defaultValue='6' prefix={<p>Number of Works</p>} />
+            <InputNumber size="md" defaultValue='6' prefix={<p>Number of Works</p>} onChange={setNumberWorks} />
           </FlexboxGrid.Item>
           <FlexboxGrid.Item colspan={4} style={padding}>
             <Checkbox>Include Best Seller</Checkbox>
